6cccdfc57735d843357f60094d1e10723ce9a52a
Viktoriia Zakorchemna <viktoriia.zakorchemna@gmail.com>
2021-02-18 20:05:51 +0100

    feat: add brackets.js task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const newArr = bracketsConfig.map((el) => el.join(''));
+
+  for (let i = 0; i < newArr.length;) {
+    if (str.includes(newArr[i])) {
+      str = str.replace(newArr[i], '');
+      i = 0;
+      continue;
+    }
+    i++;
+  }
+  if (str.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+
+};
